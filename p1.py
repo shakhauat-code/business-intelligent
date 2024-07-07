5019cc3b12cb5a8d168440789c8e4cd0c4e2e038
shakhauat-code <shakauat.hossain@doer.com.bd>
2024-07-07 18:06:09 +0600

    Update p1.py

--- p1.py
+++ p1.py
@@ -157,7 +157,7 @@ def home_page():
     agent_dep = df2.groupby('Agent_id')['deposit'].sum().reset_index()
 
 
-    col8, sp, col9 = st.columns([1, 0.1, 1])
+    col8, col9 = st.columns([1, 1])
 
     with col8:
         # Plotly bar chart

f9b6565bff82a167c548677ca01cd1b703d4dc37
shakhauat-code <shakauat.hossain@doer.com.bd>
2024-07-08 13:48:09 +0600

    Update p1.py

--- p1.py
+++ p1.py
@@ -36,14 +36,13 @@ def login_page():
     login_button = st.button("Log In")
 
     if login_button:
-        try:
-            int(password) in bdex_pass.values():
+        if int(password) in bdex_pass.values():
             st.session_state.logged_in = True
             st.session_state.page = 'Home'
             st.session_state.username = password
             st.success("Logged in successfully!")
             st.experimental_rerun()
-        except:
+        elif password == '':
             st.error("Invalid username or password.")
 
 
